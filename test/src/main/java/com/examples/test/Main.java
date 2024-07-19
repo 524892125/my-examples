ac5b7c404ec6ce7b49b5e352bc33e66864cb1c4d
DESKTOP-PIB4DJF\lisheng <524892125@QQ.COM>
2024-07-19 16:31:58 +0800

    添加翻译业务

--- test/src/main/java/com/examples/test/Main.java
+++ test/src/main/java/com/examples/test/Main.java
@@ -2,9 +2,18 @@ package com.examples.test;
 
 import com.examples.word.controller.Common;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("a.txt"));
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+
         Common.show("asd");
         System.out.println("Hello World!");
     }
