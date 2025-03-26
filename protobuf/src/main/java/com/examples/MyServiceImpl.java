6799766176ca57194276dc06abf13d31e0cc3b33
li <524892125@qq.com>
2025-03-26 15:51:25 +0800

    websocket转发服务

--- protobuf/src/main/java/com/examples/MyServiceImpl.java
+++ protobuf/src/main/java/com/examples/MyServiceImpl.java
@@ -6,8 +6,11 @@ public class MyServiceImpl extends MyServiceGrpc.MyServiceImplBase {
     @Override
     public void sayHello(Service.HelloRequest request, StreamObserver<Service.HelloResponse> responseObserver) {
         String name = request.getName();
+        System.out.println("Received request: " + name);
+        String phone = request.getPhone();
         Service.HelloResponse response = Service.HelloResponse.newBuilder()
                 .setMessage("Hello, " + name)
+                .setPhone(phone)
                 .build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
