5b6abde888e685d9c84b05650616e970aac73e06
li <524892125@qq.com>
2025-03-26 15:35:51 +0800

    websocket转发服务

--- protobuf/src/main/java/com/examples/Main.java
+++ protobuf/src/main/java/com/examples/Main.java
@@ -1,35 +1,22 @@
-package com.examples;
-
-import com.example.tutorial.protos.Person;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-public class Main {
-    public static void main(String[] args) {
-        // 创建 Person 对象
-        Person person = Person.newBuilder()
-                .setName("Alice")
-                .setEmail("alice@example.com")
-                .build();
-
-        // 序列化到文件
-        try (FileOutputStream output = new FileOutputStream("person.bin")) {
-            person.writeTo(output);
-            System.out.println("序列化成功: " + person);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 从文件反序列化
-        try (FileInputStream input = new FileInputStream("person.bin")) {
-            Person deserializedPerson = Person.parseFrom(input);
-            System.out.println("反序列化成功: " + deserializedPerson);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//package com.examples;
+//
+//import io.grpc.Server;
+//import io.grpc.ServerBuilder;
+//
+//import java.io.IOException;
+//
+////TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+//// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+//public class Main {
+//    public static void main(String[] args) throws IOException, InterruptedException {
+//        // 创建并启动 gRPC 服务器
+//        Server server = ServerBuilder.forPort(50051)
+//                .addService(new MyServiceImpl())  // 注册服务实现
+//                .build();
+//
+//        server.start();
+//        System.out.println("Server started on port 50051");
+//
+//        server.awaitTermination();
+//    }
+//}
